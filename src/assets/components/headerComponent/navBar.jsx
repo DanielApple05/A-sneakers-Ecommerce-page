c275b7d3487992a2df51bb9b43e2f969e044b9fc
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-02-05 15:30:41 +0100

    commits

--- src/assets/components/headerComponent/navBar.jsx
+++ src/assets/components/headerComponent/navBar.jsx
@@ -1,36 +1,43 @@
 import React from 'react';
+import { useState } from 'react';
 import NavBtns from './navTabs';
 import SneakIcon from '../../images/sneakShop-icon.png';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartShopping } from "@fortawesome/free-regular-svg-icons";
+import { faCartShopping, faCircleUser } from "@fortawesome/free-solid-svg-icons";
 
 const navBar = () => {
 
+  const [activeIndex, setActiveIndex] = useState();
+
   return (
-    <div className='flex'>
+    <div className='flex justify-between px-[80px] bg-gray-400'>
 
 
       <div>
         <img src={SneakIcon} alt="" className='w-[120px]' />
       </div>
 
-      <div className=' space-x-[30px] flex bg-gray-400'>
+      <div className=' space-x-[50px] flex '>
 
         {
           NavBtns.map((NavBtns, index) => (
-            <button key={index} className=''>
+            <button key={index} className={`cursor-pointer font-bold ${activeIndex === index
+              ? "border-b-2 border-white hover:border-b-white "
+              : "border-b-2 border-transparent"
+              }` }>
               {
                 NavBtns.tab
               }
             </button>
           ))
         }
-
       </div>
-
-      <div>
+      <div className='flex place-content-center gap-[20px]'>
        <button>
-        <FontAwesomeIcon icon={ faCartShopping} />
+        <FontAwesomeIcon icon={ faCartShopping } className='text-[20px]' />
+       </button>
+       <button>
+        <FontAwesomeIcon icon={ faCircleUser } className='text-[20px]' />
        </button>
       </div>
 
