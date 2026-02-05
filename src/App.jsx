32b480db019f3708710277b42c8ba202fc899d18
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-02-05 12:59:25 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Index from './assets/index'
+
+const App = () => {
+  return (
+    <div>
+      <Index></Index>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/assets/components/headerComponent/navBtn.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const navBtn =  {
+  
+}
+
+export default navBtn;
