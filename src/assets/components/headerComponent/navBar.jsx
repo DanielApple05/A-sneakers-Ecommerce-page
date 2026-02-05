c95d4701b1857e96625e463716b03e5805a04e4a
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-02-05 14:51:46 +0100

    header commit

--- src/assets/components/headerComponent/navBar.jsx
+++ src/assets/components/headerComponent/navBar.jsx
@@ -1,19 +1,39 @@
 import React from 'react';
-import NavBtns from './navTabs'
+import NavBtns from './navTabs';
+import SneakIcon from '../../images/sneakShop-icon.png';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-regular-svg-icons";
 
 const navBar = () => {
 
   return (
-    <div>
-     {
-      NavBtns.map((NavBtns, index) => (
-       <button key={index}>
-       {
-        NavBtns.tab
-       }
-       </button> 
-      ))
-     }
+    <div className='flex'>
+
+
+      <div>
+        <img src={SneakIcon} alt="" className='w-[120px]' />
+      </div>
+
+      <div className=' space-x-[30px] flex bg-gray-400'>
+
+        {
+          NavBtns.map((NavBtns, index) => (
+            <button key={index} className=''>
+              {
+                NavBtns.tab
+              }
+            </button>
+          ))
+        }
+
+      </div>
+
+      <div>
+       <button>
+        <FontAwesomeIcon icon={ faCartShopping} />
+       </button>
+      </div>
+
     </div>
   );
 }
