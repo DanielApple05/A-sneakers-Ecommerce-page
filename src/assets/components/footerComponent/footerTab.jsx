ef5a7db362a290158de7429a31b51bf9bbfc4533
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-02-10 09:21:05 +0100

    FIX: image error

--- src/assets/components/footerComponent/footerTab.jsx
+++ src/assets/components/footerComponent/footerTab.jsx
@@ -9,10 +9,10 @@ import visaPay from '../../images/visa-icon.png';
 import masterPay from '../../images/mastercard-icon.png';
 import ex_Pay from '../../images/american-EX-icon.png';
 import applePAy from '../../images/applePay-icon.png';
-import payPal from '../../images/paypal-icon.png'
-import brandLogo1 from '../../images/Brand-logo-1.jpg'
-import brandLogo2 from '../../images/brand-logo-2.jpg'
-import brandLogo3 from '../../images/brand-logo-3.jpg'
+import payPal from '../../images/paypal-icon.png';
+import brandLogo1 from '../../images/Brand-Logo-1.jpg';
+import brandLogo2 from '../../images/brand-logo-2.jpg';
+import brandLogo3 from '../../images/brand-logo-3.jpg';
 
 const footerTab = {
   links: [
