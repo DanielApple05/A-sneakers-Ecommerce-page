cb9d9936a29d3765c5d1841aec95f88cc7a619df
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-02-11 04:18:41 +0100

    ADD: Gender filter active

--- src/assets/components/shopComponent/shop-gallery.jsx
+++ src/assets/components/shopComponent/shop-gallery.jsx
@@ -1,17 +1,28 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+
 
 const ShopGallery = ({ sneaker }) => {
-  const folders = ["All", "Male", "Female"]
-  const shoeBrands = ["Adidas", "Air Jordan", "Nike"]
+  const allShoes = sneaker;
+
+  const folders = ["all", "male", "female"]
+  const shoeBrands = ["adidas", "jordans", "nikeAirforce"]
+  const [activeFilter, setActiveFilter] = useState("all");
+  const filteredShoes =
+    activeFilter === "all"
+      ? allShoes
+      : allShoes.filter(shoe => shoe.gender === activeFilter);
+
+
 
   return (
     <div className='px-20 py-10  bg-[#FDF6EC]'>
       <h2 className="mb-6 text-[30px] font-bold">
         Shop
       </h2>
-      <div className="flex">
-        <div className='bg-white w-8/12 h-80 mr-8'>
+      <div className="flex ">
+        <div className='bg-white w-8/12 h-screen mr-8 sticky'>
           <div className='font-semibold border-b p-3 flex items-center justify-between'>
             <h5 className=''>
               Filters
@@ -24,10 +35,11 @@ const ShopGallery = ({ sneaker }) => {
               <FontAwesomeIcon icon={faAngleRight} className="" />
             </div>
             {shoeBrands.map((brand) => (
-              <div className="">
+              <div key={brand} className="">
                 <input type="checkbox" />
-                <button key={brand} className="text-start p-2 "> {brand}</button>
+                <button className="text-start p-2">{brand}</button>
               </div>
+
             ))
             }
           </div>
@@ -36,30 +48,24 @@ const ShopGallery = ({ sneaker }) => {
         <div>
           <div className='space-x-4 text-white pb-4'>
             {folders.map((folder) => (
-              <button key={folder} className='bg-red-500 py-2 px-6 rounded-xl'>{folder}</button>))}
+              <button key={folder} onClick={() => setActiveFilter(folder)} className='bg-red-500 py-2 px-6 rounded-xl cursor-pointer hover:bg-red-800'>{folder}</button>))}
           </div>
 
           <div className='grid grid-cols-4 gap-6 '>
-            {sneaker.map((shoe) => (
-              <div key={shoe.id} className='grid bg-gray-400 rounded-xl shadow-xl'>
-                <div>
-                  <img src={shoe.image} alt="" className=' rounded-t-xl' />
+            {filteredShoes.map((shoe) => (
+              <div key={shoe.id} className="grid bg-gray-400 rounded-xl shadow-xl">
+                <img src={shoe.image} className="rounded-t-xl" />
+
+                <div className="text-center pt-2">
+                  <h3 className="font-semibold">{shoe.name}</h3>
+                  <p>${shoe.price}</p>
                 </div>
 
-                <div className='text-center pt-2'>
-                  <h3 className='font-semibold'>
-                    {shoe.name}
-                  </h3>
-                  <p className='text-[18px]'>
-                    ${shoe.price}
-                  </p>
-                </div>
-                <button className="bg-blue-400 text-white m-7 font-bold p-3 rounded-xl hover:bg-blue-600 cursor-pointer">
+                <button className="bg-blue-400 text-white m-7 font-bold p-3 rounded-xl">
                   Add to Cart
                 </button>
               </div>
-            ))
-            }
+            ))}
           </div>
         </div>
       </div>
