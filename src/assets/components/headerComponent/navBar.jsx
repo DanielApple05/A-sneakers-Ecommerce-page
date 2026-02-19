a8f3966db30b62e342833808751bb63f9fc7f0c2
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-02-19 11:41:01 +0100

    header linked was replaced with link to allow active bar to stay active

--- src/assets/components/headerComponent/navBar.jsx
+++ src/assets/components/headerComponent/navBar.jsx
@@ -6,10 +6,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleUser, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { Link } from 'react-router-dom';
 import CartBar from '../cartComponent/addToCart'
+import { NavLink } from 'react-router-dom';
+
 
 const navBar = () => {
-
-  const [activeIndex, setActiveIndex] = useState(0);
 
   return (
     <div className='flex justify-between px-20 bg-black/90 min-h-20 max-h-25 max-w-12/12 '>
@@ -19,23 +19,23 @@ const navBar = () => {
         <img src={SneakIcon} alt="" className='  w-25 rounded-xl' />
       </div>
 
-      <div className=' space-x-12.5 flex pt-10 text-sm'>
-        {
-          NavBtns.map((navBtn, index) => (
-            <Link
-              key={index}
-              to={navBtn.path}
-              onClick={() => setActiveIndex(index)}
-              className={`cursor-pointer font-bold text-gray-400 hover:text-white transition ${activeIndex === index
-                ? "border-b-2 border-white text-white"
-                : ""
-                }`}
-            >
-             <p>{navBtn.tab}</p> 
-            </Link>
-          ))
-        }
-      </div>
+     <div className='space-x-12.5 flex pt-10 text-sm'>
+  {NavBtns.map((navBtn, index) => (
+    <NavLink
+      key={index}
+      to={navBtn.path}
+      className={({ isActive }) =>
+        `cursor-pointer font-bold transition ${
+          isActive
+            ? "border-b-2 border-white text-white"
+            : "text-gray-400 hover:text-white"
+        }`
+      }
+    >
+      <p>{navBtn.tab}</p>
+    </NavLink>
+  ))}
+</div>
       <div className="text-gray-400 flex gap-7.5 items-center  ">
         <button >
           <FontAwesomeIcon icon={faMagnifyingGlass} className='text-[20px] cursor-pointer hover:text-white transition' />
--- src/assets/components/headerComponent/navTabs.jsx
+++ src/assets/components/headerComponent/navTabs.jsx
@@ -1,13 +1,9 @@
-import React from 'react';
-
-const navTabs =  [
-
-{ tab: "Home",  path: "/index"},
-{ tab: "Shop",  path: "/shop"},
-{ tab: "Contact"},
-{ tab: "Collections"},
-{ tab: "Blog"},
-
-]
+const navTabs = [
+  { tab: "Home", path: "/index" },
+  { tab: "Shop", path: "/shop" },
+  { tab: "Contact", path: "/contact" },
+  { tab: "Collections", path: "/collections" },
+  { tab: "Blog", path: "/blog" },
+];
 
 export default navTabs;
